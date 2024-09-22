2cafe4d986b667b8de6fa2591cdee7912efc1c48
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-09-22 23:34:04 +0545

    trying to screate a simple box geometry my slf

--- threeyt/main.js
+++ threeyt/main.js
@@ -1,21 +1,69 @@
-import * as THREE from 'three';
+// import * as THREE from "three";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+// camera.position.z = 5;
+
+// scene.add(camera);
+
+// let box = new THREE.BoxGeometry(2, 2, 2);
+// let material = new THREE.MeshBasicMaterial({
+//   color: 0x00ff00,
+//   wireframe: true,
+// });
+// let mesh = new THREE.Mesh(box, material);
+
+// scene.add(mesh);
+
+// let renderer = new THREE.WebGLRenderer({
+//   canvas: document.querySelector("#draw"),
+// });
+
+// renderer.setSize(window.innerWidth, window.innerHeight);
+
+// window.addEventListener("resize", () => {
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+// });
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.autoRotate  = true;
+// controls.autoRotateSpeed = 11;
+// controls.enableZoom = true;
+
+// function animate() {
+//   window.requestAnimationFrame(animate);
+//   renderer.render(scene, camera);
+
+//   controls.update();
+// }
+
+// animate();
+
+
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
-
 scene.add(camera);
 
-
-let box = new THREE.BoxGeometry(2,2,2);
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-let mesh = new THREE.Mesh(box, material);
-
+const box = new THREE.BoxGeometry(3, 3, 3);
+const material = new THREE.MeshBasicMaterial({color: 0x00ff00, wireframe: true});
+const mesh = new THREE.Mesh(box, material);
 scene.add(mesh);
 
-let renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#draw")});
+const renderer = new THREE.WebGLRenderer({canvas: document.querySelector('#draw')});
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 
 window.addEventListener("resize", () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -23,13 +71,17 @@ window.addEventListener("resize", () => {
     camera.updateProjectionMatrix();
 });
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.autoRotate = true;
+controls.autoRotateSpeed = 11;
+controls.enableZoom = true;
+
+
 function animate(){
     window.requestAnimationFrame(animate);
     renderer.render(scene, camera);
-    mesh.rotation.y += 0.01;
-    mesh.rotation.z += 0.01;
-    mesh.rotation.x += 0.01;
+    controls.update();
 }
 
 animate();
-
